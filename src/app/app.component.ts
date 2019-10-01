8f72022703c5107ff33d7e2325ece0514a560eb5
Steve <Steve@DESKTOP-PGO5BC4>
2019-10-01 18:05:31 -0500

    Chapter 8 - not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 @Component({
-  selector: "app",
-  template: "<store></store>"
+    selector: "app",
+    template: "<router-outlet></router-outlet>"
 })
 export class AppComponent { }
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
+import { Cart } from "../model/cart.model";
+import { Router } from "@angular/router";
 @Component({
     selector: "store",
     templateUrl: "store.component.html"
@@ -9,7 +11,9 @@ export class StoreComponent {
     public selectedCategory = null;
     public productsPerPage = 4;
     public selectedPage = 1;
-    constructor(private repository: ProductRepository) {}
+    constructor(private repository: ProductRepository,
+        private cart: Cart,
+        private router: Router) { }
     get products(): Product[] {
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage
         return this.repository.getProducts(this.selectedCategory)
@@ -31,5 +35,9 @@ export class StoreComponent {
     get pageCount(): number {
         return Math.ceil(this.repository
             .getProducts(this.selectedCategory).length / this.productsPerPage)
+    }
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
+        this.router.navigateByUrl("/cart");
     }
 }
